43444c8bcc452a1da3a4a3de6823c9716cfe3ea4
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2019-07-05 08:58:54 -0500

    modified 013.cpp

--- Chapter 05/013.cpp
+++ Chapter 05/013.cpp
@@ -12,11 +12,11 @@ int main()
          << "Enter a number to begin and enter -99\n"
          << "to quit the program."
          << "Enter a value: ";
-    cin >> num; // Error 
+    cin >> num; // Error Checking needed 
 
     min = max = num;
 
-    while (num != -99)
+    while (num != SENTINEL_VALUE)
     {
         if (num < min)
             min = num;
@@ -25,12 +25,12 @@ int main()
             max = num;
 
         cout << "Enter a value: ";
-        cin >> num;
+        cin >> num; // Error Checking needed 
         
     }
     
     cout << "Smallest number is: " << min << endl;
     cout << "Largest number is: "  << max  << endl;
-
+    
     return 0;
 }
